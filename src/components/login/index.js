08c8b29089dc5abf053d58f84fbce10c939653db
ollyvergithub <ollyverottoboni@gmail.com>
2023-03-26 10:07:47 -0300

    Instala componente react-facebook

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -60,7 +60,7 @@ export const Login = () => {
             <FacebookLogin
                 textButton="LOGIN WITH FACEBOOK"
                 appId= "747336750353989"
-                fields="name,email,picture"
+                fields="first_name,email,picture"
                 callback={fbResponse}
             />
             <br/>
