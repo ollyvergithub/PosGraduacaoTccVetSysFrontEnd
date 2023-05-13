377d8ddd5531529684f6bd97e5d896f65d63f0a2
ollyvergithub <ollyverottoboni@gmail.com>
2023-05-13 12:27:57 -0300

    Finaliza a construção da aplicação

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# PosGraduacaoTccVetSysFrontEnd
+# Vet Sys
+
+## Repositório utilizado para o FrontEnd da aplicação Vet Sys - Sistema de Gerenciamento de Clínicas Veterinárias. Desenvolvida em ReactJS.
+
+### Vídeo de apresentação
+
+<https://www.youtube.com/watch?v=82jaOxmkc_Y>
+
+### Para desenvolver
+
+I)  Clone o repositório.
+```console
+$ git clone https://github.com/ollyvergithub/PosGraduacaoTccVetSysFrontEnd.git
+```
+
+II. Instale as dependências.
+
+```console
+$ npm i
+```
+
+III. Rode a aplicação.
+
+```console
+$ npm start
+```
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -20,8 +20,49 @@ export const Home = () => {
                     </div>
                     <div className='col-md-8'>
                         <h3 className='text-black'>BEM – VINDO AO VET SYS </h3>
-                        <h6><strong>Escolha as uma das opções acima no menu para começar.</strong></h6>
-                        <p>No entanto, não podemos esquecer que o início da atividade geral de formação de atitudes desafia a capacidade de equalização dos níveis de motivação departamental. O que temos que ter sempre em mente é que a valorização de fatores subjetivos afeta positivamente a correta previsão do retorno esperado a longo prazo. Evidentemente, o desenvolvimento contínuo de distintas formas de atuação exige a precisão e a definição do sistema de participação geral. As experiências acumuladas demonstram que a estrutura atual da organização auxilia a preparação e a composição dos relacionamentos verticais entre as hierarquias. Do mesmo modo, a hegemonia do ambiente político faz parte de um processo de gerenciamento dos métodos utilizados na avaliação de resultados.</p>
+                        <h5><strong>Escolha uma das opções acima no menu para começar.</strong></h5>
+                        <p className='mb-0 mt-3'><strong>POLÍTICA DE PRIVACIDADE</strong></p>
+                        <p className='mb-2'>A presente Política de Privacidade regula a coleta, o uso e o armazenamento de informações pessoais coletadas através do uso da aplicação VetSys desenvolvida por Ollyver Ottoboni. Ao utilizar esta aplicação, você concorda com os termos desta política.</p>
+                        <p className='mb-0'><strong>Coleta de informações pessoais</strong></p>
+
+                        <p className='mb-1'>Através da utilização da aplicação, podem ser coletadas as seguintes informações pessoais:</p>
+
+                        <ul>
+                            <li>Nome completo</li>
+                            <li>Endereço de e-mail</li>
+                            <li>Informações de contato (telefone, endereço)</li>
+                            <li>Informações de pagamento (quando aplicável)</li>
+                            <li>Outras informações que o usuário escolher fornecer</li>
+                        </ul>
+
+
+                        <p className='mb-1'>Essas informações são coletadas apenas com o seu consentimento explícito e serão usadas apenas para os fins para os quais foram coletadas. Elas podem ser utilizadas para:</p>
+
+                        <ul>
+                            <li>Prestar serviços e informações sobre a aplicação</li>
+                            <li>Processar pagamentos (quando aplicável)</li>
+                            <li>Fornecer suporte técnico</li>
+                            <li>Enviar informações promocionais ou newsletters (quando autorizado pelo usuário)</li>
+                        </ul>
+
+                        <p className='mb-1'><strong>Uso de cookies</strong></p>
+                        <p className='mb-2'>A aplicação pode usar cookies para melhorar a experiência do usuário e coletar informações sobre o uso da aplicação. Essas informações podem incluir o endereço IP do usuário, o tipo de dispositivo utilizado, o navegador da web utilizado, entre outras informações.</p>
+
+                        <p className='mb-1'><strong>Armazenamento e segurança das informações</strong></p>
+
+                        <p className='mb-2'>Todas as informações coletadas pela aplicação serão armazenadas em servidores seguros e protegidas por medidas de segurança apropriadas. As informações coletadas não serão compartilhadas com terceiros, exceto quando necessário para prestar serviços relacionados à aplicação.</p>
+
+                        <p className='mb-1'><strong>Direitos do usuário</strong></p>
+
+                        <p>O usuário tem o direito de solicitar informações sobre quais dados pessoais a aplicação armazena e processa, bem como solicitar a exclusão dessas informações, se assim desejar. Também é possível atualizar ou corrigir as informações pessoais fornecidas.</p>
+
+                        <p className='mb-1'><strong>Alterações na política de privacidade</strong></p>
+
+                        <p>A VetSys se reserva o direito de modificar esta política de privacidade a qualquer momento, sem aviso prévio. As alterações serão publicadas nesta página e serão efetivas imediatamente após a publicação.</p>
+
+                        <p className='mb-1'><strong>Entre em contato</strong></p>
+
+                        <p>Se você tiver alguma dúvida ou preocupação sobre a Política de Privacidade, entre em contato conosco pelo e-mail <a href='mailto:ollyverottoboni@gmail.com'>ollyverottoboni@gmail.com</a></p>
                     </div>
                 </div>
             </div>
